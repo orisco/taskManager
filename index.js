5fbfe2e28c4c10a64c6bb043e9d1da11f5cb2349
Ori Sayag <sayagori@gmail.com>
2021-04-06 10:32:00 +0300

    add

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.delete("/lists/:listId/tasks/all", (req, res) => {
   });
 // }
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log("running on " + PORT)
